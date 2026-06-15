36c8b0dbbc7bbbf3746aff6754aa8acc0be1ba96
MohsenSami <73436220+mohsensami@users.noreply.github.com>
2026-06-15 22:06:50 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 export default function App() {
+  });
+// useThrottle
+  const throttle = useThrottle(1000);
+  const myCallback = throttle(() => {
+    // api call ...
+  });
+// useDebounce
   const debounce = useDebounce(1000);
-
   const handleSearch = debounce(() => {
     // Executes after 1000ms
   });
   return (
-    <input onChange={handleSearch} /> 
+    <>
+      <input onChange={handleSearch} /> 
+      <button onClick={myCallback}>Check</button>
+    </>
   );
 }
